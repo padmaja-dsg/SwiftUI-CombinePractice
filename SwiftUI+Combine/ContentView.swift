aaa4d401e5f08383776d3d52bf3c7881f52a2d1e
Padmaja Unnam <padmaja.unnam@dcsg.com>
2021-06-12 00:59:22 -0500

    Initial Commit

---
+++ SwiftUI+Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI+Combine
+//
+//  Created by Padmaja Unnam on 6/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI+Combine/SwiftUI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CombineApp.swift
+//  SwiftUI+Combine
+//
+//  Created by Padmaja Unnam on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
